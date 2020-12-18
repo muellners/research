d4b96fcebd3bf48881fadbf644d4f0152275bc2f
saransh <saransh@muellners.org>
2020-12-18 19:47:36 +0530

    about update

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -60,39 +60,22 @@ const About: React.FC = () => (
                   About Research @ Muellenrs <br /> GitHub: <a href="https://github.com/muellners/research"></a>
                 </h5>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc commodo finibus leo,
-                  non tempus magna vehicula ac. Maecenas mollis ante finibus pharetra imperdiet.
-                  Maecenas in aliquam purus. Nam et massa a nulla fermentum dapibus sit amet in
-                  neque. Ut ipsum ipsum, rhoncus a sodales pellentesque, interdum a elit. Nullam
-                  aliquam tellus nibh, eget laoreet dui aliquet non. Vestibulum malesuada ante at
-                  diam tempus, ac interdum risus scelerisque. Sed ipsum neque, vulputate porta diam
-                  eget, consequat blandit nulla. Integer volutpat velit vitae purus lacinia aliquam.
-                  Integer bibendum ipsum vitae magna pulvinar, nec vehicula dolor vulputate. Nulla
-                  eu massa id orci placerat finibus vel sit amet eros. Vestibulum quis consequat
-                  massa. Sed sagittis sollicitudin massa at commodo. Praesent diam nisi, imperdiet
-                  posuere eleifend nec, blandit ac massa.
+                  As an organization, we maintain a portfolio of research projects driven by
+                  fundamental research, new product innovation, product contribution and infrastructure goals,
+                   while providing individuals and teams the freedom to emphasize specific types of work.
+                   We strive to create an environment conducive to many different types of research across
+                   many different time scales and levels of risk.
                 </p>
                 <p>
-                  Vestibulum semper pretium ipsum nec congue. Ut ac eros nisi. Donec leo sem,
-                  aliquam mollis sapien ultrices, dapibus congue diam. Proin viverra dapibus
-                  blandit. Ut mauris tellus, tristique id felis vel, venenatis vestibulum nunc. Nam
-                  molestie pulvinar nibh, eget egestas augue. Maecenas tellus arcu, mattis ut ipsum
-                  non, sollicitudin convallis nunc. Donec nec neque tristique, aliquet lacus id,
-                  laoreet nunc. Cras dapibus nisi nulla, ullamcorper faucibus neque suscipit ac.
-                  Donec eget orci venenatis justo lobortis volutpat. Proin vel placerat nisl.
-                  Integer arcu nunc, sodales eu fringilla non, aliquam non diam. Cras placerat,
-                  massa et faucibus pretium, ante elit tincidunt tellus, tristique ultricies velit
-                  quam et massa.
-                </p>
-                <p>
-                  In nunc lacus, dapibus vitae lacus sit amet, efficitur iaculis neque. Suspendisse
-                  ut tellus quis leo vestibulum tincidunt. Aenean nec enim ac dolor lacinia semper.
-                  Ut sed laoreet libero. Nunc elementum sollicitudin accumsan. Nunc eu augue neque.
-                  Proin a tortor nibh. Cras eu nisl ornare sapien feugiat pellentesque. Mauris
-                  dignissim vel quam eu pellentesque. Integer sit amet posuere quam, eu ullamcorper
-                  odio. Nullam a lacus tempus sapien dignissim ullamcorper. In hac habitasse platea
-                  dictumst. Proin quis massa aliquam, feugiat tortor sit amet, tincidunt urna. Donec
-                  posuere pulvinar lectus, ac semper ipsum vulputate quis.
+                  Our goal is to create a research environment rich in opportunities for product impact, to build a
+                  product environment that actively benefits from research, and to provide our staff the freedom to
+                  work on important research problems that are not tied to immediate product needs.
+                  Some of the most exciting research enables new products, or even new businesses,
+                  that we cannot imagine today. Given the diversity of
+                  research projects that we pursue, we have found it useful to define four types of work to
+                   help crystalize the goals of projects and allow us to measure progress.
+                   Although the organization as a whole needs to include all four types of work, teams or
+                   individuals typically emphasize a subset of them.
                 </p>
               </div>
             </PostFullContent>
