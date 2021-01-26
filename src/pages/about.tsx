ce26d3841cd20778c6a9cf73f9d8d6cbee08ad58
saransh <saransh@muellners.org>
2021-01-26 21:32:35 +0530

    Changed Readme

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -57,25 +57,23 @@ const About: React.FC = () => (
             <PostFullContent className="post-full-content">
               <div className="post-content">
                 <h5>
-                  About Research @ Muellners <br /> GitHub: <a href="https://github.com/muellners/research"></a>
+                  About Research by Muellners <br /> GitHub: <a href="https://github.com/muellners/research"></a>
                 </h5>
                 <p>
-                  As an organization, we maintain a portfolio of research projects driven by our
-                  fundamental research goals, new product innovations, product contribution, and infrastructure goals.
-                   
-                   We strive to create an environment conducive to many different types of research across
-                   many different time scales and levels of risk.
+                As an organization, we maintain a portfolio of research projects driven by
+                fundamental research, new product innovation, product contribution and infrastructure goals,
+                 while providing individuals and teams the freedom to emphasize specific types of work.
+                 We strive to create an environment conducive to many different types of research across
+                 many different time scales and levels of risk.
                 </p>
                 <p>
-                  Our goal is to create a research environment, rich in opportunities for product impact, to build a
-                  product environment that actively benefits from research, and to provide our staff the freedom to
-                  work on important research problems that are not tied to immediate product needs.
-                  Some of the most exciting research enables new products, or even new businesses,
-                  that we cannot imagine today. Given the diversity of
-                  research projects that we pursue, we have found it useful to define four types of work to
-                   help crystalize the goals of projects and allow us to measure progress.
-                   Although the organization as a whole needs to include all four types of work, teams or
-                   individuals typically emphasize a subset of them.
+                Our goal is to create a research environment rich in opportunities for product impact, to build a
+                product environment that actively benefits from research, and to provide our staff the freedom to
+                work on important research problems that are not tied to immediate product needs.
+                Some of the most exciting research enables new products, or even new businesses,
+                that we cannot imagine today. Given the diversity of
+                research projects that we pursue, we have found it useful to define four types of work to
+                 help crystalize the goals of projects and allow us to measure progress.
                 </p>
               </div>
             </PostFullContent>
