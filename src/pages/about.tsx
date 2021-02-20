e9fdd444eea65f95731e1c0754f3d90199c77afa
Ankit@Muellners <53999665+Ankit-muellner@users.noreply.github.com>
2021-02-20 14:50:53 +0100

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -57,23 +57,30 @@ const About: React.FC = () => (
             <PostFullContent className="post-full-content">
               <div className="post-content">
                 <h5>
-                  About Research by Muellners <br /> GitHub: <a href="https://github.com/muellners/research"></a>
+                  About "Research Blog"  
                 </h5>
                 <p>
                 As an organization, we maintain a portfolio of research projects driven by our zeal for
                 fundamental research, new product innovations, open contribution and sustainable goals.
-                 </p><p>
+                 </p>
+                <p>
                  We strive to create an environment helpful to many different types of projects across
                  many different time scales, sectors and industries.
                 </p>
                 <p>
                 Our goal is to create a research environment, rich in opportunities & product impact, to build a
                 product environment that actively benefits from this research, and to provide our team the freedom to
-                  work and share their ideas on problems of humanity.</p><p>
+                  work and share their ideas on problems of humanity.
+                </p>
+                <p>
                 Some of the most exciting research enables new products, or even new businesses,
                 that we cannot imagine today. Given the diversity of our endevours
                 that we pursue, we have found it useful to publish our work on this blog.
                 </p>
+                <h3>Impressum </h3> 
+                <p> Muellners® ApS; Copenhagen, Denmark CVR: 41548304;</p>
+                <p>Muellners® New Delhi, India CIN: U72900DL2019PTC344870;</p>
+                <p> Muellners® Foundation Copenhagen, Denmark CVR:41008407</p> 
               </div>
             </PostFullContent>
           </article>
