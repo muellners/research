cf44fa58136fc3755e06d122091d92524daa2ac9
Yatish Kelkar <yatish1606@gmail.com>
2021-07-02 13:01:57 +0530

    Updates

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,7 +13,7 @@ export const Footer: React.FC = () => {
     <footer css={[outer, SiteFooter]}>
       <div css={[inner, SiteFooterContent]}>
         <section className="copyright">
-        <!--  <Link to="/">{config.title}</Link> &copy;--> {new Date().getFullYear()}{' '}
+         {/* <Link to="/">{config.title}</Link> &copy; */}{new Date().getFullYear()}{' '} 
           {config.footer && (
             <Link to="/">
               | {config.title} {config.footer}
